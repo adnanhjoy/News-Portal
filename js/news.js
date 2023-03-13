62dc221937107f5f801ce2b4a4189988b81f452c
engadnanhossain <engr.adnanhossain@gmail.com>
2023-03-13 11:04:10 +0600

    Added News Category

--- js/news.js
+++ js/news.js
@@ -10,14 +10,37 @@ const categoryDisplay = categories => {
         const li = document.createElement('li');
         li.classList.add('nav-item');
         li.innerHTML = `
-        <a class="nav-link px-3" aria-current="page" href="#">${category.category_name}</a>
+        <a onclick="categoryData('${category.category_id ? category.category_id : 'No News Found'}')" class="nav-link px-3">${category.category_name}</a>
         `;
         menuBar.appendChild(li);
-        console.log(category)
+        // console.log(category)
     })
 };
 
-const categoryData = (news_id) => {
-    fetch(`https://openapi.programming-hero.com/api/news/category/news_id`)
-}
+const categoryData = news_id => {
+    fetch(`https://openapi.programming-hero.com/api/news/category/${news_id}`)
+        .then(res => res.json())
+        .then(data => categoryDataDisplay(data.data))
+};
+
+const categoryDataDisplay = newses => {
+    const newsContainer = document.getElementById('news-container');
+    newsContainer.textContent = '';
+    newses.forEach(news => {
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `
+            <div class="card">
+                <img src="${news ? news.thumbnail_url : 'No news found'}" class="card-img-top" alt="news-thumbnail">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in
+                        to additional content. This content is a little bit longer.</p>
+                </div>
+            </div>
+        `;
+        newsContainer.appendChild(div)
+        console.log(news)
+    })
+};
 categoryLoad()
